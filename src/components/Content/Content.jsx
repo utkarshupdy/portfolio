b0b2d24644472ee4a540b24c869efa847409de74
Utkarsh Kumar Upadhyay <ukupadhyay6789@gmail.com>
2024-12-30 16:57:43 +0530

    content updated

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -35,10 +35,10 @@ const Content = () => (
                 </p>
 
                 <div className="redes-sociales">
-                    <a href="https://www.linkedin.com/in/esteban-nahuel-carrizo-69715422b/" target="_blank" rel="noopener noreferrer" data-aos="fade-up" data-aos-delay="1200"><i className="fab fa-linkedin"></i></a>
-                    <a href="https://github.com/Nahuel61920/" target="_blank" rel="noopener noreferrer" data-aos="fade-up" data-aos-delay="1400"><i className="fab fa-github"></i></a>
-                    <a href="https://www.instagram.com/nahuelcarrizolc/" target="_blank" rel="noopener noreferrer" data-aos="fade-up" data-aos-delay="1600"><i className="fab fa-instagram"></i></a>
-                    <a href="https://api.whatsapp.com/send?phone=5493815360966" target="_blank" rel="noopener noreferrer" data-aos="fade-up" data-aos-delay="1800"><i className="fab fa-whatsapp"></i></a>
+                    <a href="https://linkedin.com/in/utkarsh-kumar-upadhyay-18685b24a/" target="_blank" rel="noopener noreferrer" data-aos="fade-up" data-aos-delay="1200"><i className="fab fa-linkedin"></i></a>
+                    <a href="https://github.com/utkarshupdy/" target="_blank" rel="noopener noreferrer" data-aos="fade-up" data-aos-delay="1400"><i className="fab fa-github"></i></a>
+                    <a href="https://www.instagram.com/utkarshupdy.exe/" target="_blank" rel="noopener noreferrer" data-aos="fade-up" data-aos-delay="1600"><i className="fab fa-instagram"></i></a>
+                    <a href="https://api.whatsapp.com/send?phone=7309466387" target="_blank" rel="noopener noreferrer" data-aos="fade-up" data-aos-delay="1800"><i className="fab fa-whatsapp"></i></a>
                 </div>
                 <div className="wrapper">
                     <a className="button" href="https://www.linkedin.com/in/esteban-nahuel-carrizo-69715422b/" target="_blank" rel="noopener noreferrer" data-aos="fade-up" data-aos-delay="1200">
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () =>{
                     <p >&copy; {fetch}. All Rights Reserved.</p>
                 </div>
                 <div className="redes-sociales">
-                    <a href="linkedin.com/in/utkarsh-kumar-upadhyay-18685b24a/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
+                    <a href="https://linkedin.com/in/utkarsh-kumar-upadhyay-18685b24a/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
                     <a href="https://github.com/utkarshupdy/" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
                     <a href="https://www.instagram.com/utkarshupdy.exe/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
                     <a href="https://api.whatsapp.com/send?phone=7309466387" target="_blank" rel="noopener noreferrer"><i className="fab fa-whatsapp"></i></a>
